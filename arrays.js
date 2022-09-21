8503571755ceb88f012a12def94150b2d2a71ee3
Michael Lobato <mike.lobato.wft@gmail.com>
2022-09-21 09:03:27 -0700

    arrayMethods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var friends = ["samer", "sara", "bryan", "victoria"];
+//removes "samer" from the array, reassigning the index position of every remaining element.
+friends.pop();
 var weightLog = [143, 147, 152, 151];
+//removing elements outside of the index numbers listed, elements of and inside the index numbers will remain. ex [143, 147, 152]
+weightLog.slice(0,2);
 var workout = [true, false, false, true];
+//adding and additional element to the front of the array with the same value as the index number of 1. Index numbers will be reassigned.
+workout.unshift(1);
